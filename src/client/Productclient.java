39d2cb96e11713ce890a8b0dc352708fd196403c
User <User@DESKTOP-93E0AAM>
2022-08-15 19:43:56 +0900

    first commit

---
+++ src/client/Productclient.java
@@ -0,0 +1,13 @@
+package client;
+
+import controller.ProductController;
+
+
+public class Productclient {
+	public static void main(String[] args) {
+		ProductController app =new ProductController();
+		
+		app.startApp();
+	}
+
+}
